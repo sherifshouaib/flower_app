0a7ca98933a2dcd31b899c27bf9b4ffd8bd55f1b
sherifshouaib <sherifshouaib22@gmail.com>
2024-06-20 07:53:17 +0300

    flutter_drawer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,6 +7,54 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/nature.webp"),
+                        fit: BoxFit.cover),
+                  ),
+                  currentAccountPicture: CircleAvatar(
+                      radius: 55,
+                      backgroundImage:
+                          AssetImage("assets/images/john cena.jpeg")),
+                  accountName: Text("ali Hassan",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      )),
+                  accountEmail: Text("ali@yahoo.com"),
+                ),
+                ListTile(
+                    title: Text("Home"),
+                    leading: Icon(Icons.home),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("My products"),
+                    leading: Icon(Icons.add_shopping_cart),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("About"),
+                    leading: Icon(Icons.help_center),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("Logout"),
+                    leading: Icon(Icons.exit_to_app),
+                    onTap: () {}),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 12),
+              child: Text("Developed by Ali Hassan © 2024",
+                  style: TextStyle(fontSize: 16)),
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         actions: [
           Row(
@@ -18,7 +66,8 @@ class Home extends StatelessWidget {
                     child: Container(
                       child: Text(
                         '8',
-                        style: TextStyle(fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
+                        style: TextStyle(
+                            fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
                       ),
                       padding: EdgeInsets.all(5),
                       decoration: BoxDecoration(
