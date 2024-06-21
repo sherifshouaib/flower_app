28b79b6b35a78d1589808f0542ca589335fdc446
sherifshouaib <sherifshouaib22@gmail.com>
2024-06-21 05:26:40 +0300

    flutter gridview.builder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -96,6 +96,49 @@ class Home extends StatelessWidget {
         backgroundColor: appbarGreen,
         title: Text('Home'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 22),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 33),
+          itemCount: 4,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {},
+              child: GridTile(
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: -3,
+                      bottom: -9,
+                      right: 0,
+                      left: 0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(55),
+                        child: Image.asset('assets/images/plant_image.jpeg'),
+                      ),
+                    ),
+                  ],
+                ),
+                footer: GridTileBar(
+                  backgroundColor: Color.fromARGB(66, 73, 127, 110),
+                  trailing: IconButton(
+                      color: Color.fromARGB(255, 62, 94, 70),
+                      onPressed: () {},
+                      icon: Icon(Icons.add)),
+                  leading: Text("\$12.99"),
+                  title: Text(
+                    "",
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
